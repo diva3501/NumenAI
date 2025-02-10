c9a1c84d9d898c6ceccad46bbeabd43e6642d96a
Divakar G <divakar3501@gmail.com>
2025-02-10 15:35:17 +0530

    Numen AI

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+const api_key = "API_KEY_HERE";
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA7DxKo-qJcakHfYc5xFQh167iMkcyff_E",
+    authDomain: "personaltaskmanager-382c0.firebaseapp.com",
+    projectId: "personaltaskmanager-382c0",
+    storageBucket: "personaltaskmanager-382c0.firebasestorage.app",
+    messagingSenderId: "136317306916",
+    appId: "1:136317306916:web:74a2cd1a109cf9dacab0b1",
+    measurementId: "G-HJHTKPVEC8"
+  }
+};
